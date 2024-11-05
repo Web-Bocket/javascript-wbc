1f0dad42a7fc3162ee4aa17fa4bb7a742887359d
Web_Bocket <131114486+Web-Bocket@users.noreply.github.com>
2024-11-05 05:34:00 +0000

    condition

--- README.md
+++ README.md
@@ -129,6 +129,56 @@ F  T   T
 F  F   F  
 
 
+conditional statement :- 
+
+- to implement some condition in the code. 
+- there are 3 types of conditional statements are there
+
+1. if condition :- 
+
+- if condition is true then statement is true.
+- syntax :- 
+if (condition){
+    statement
+}
+
+2. if-else condition :- 
+
+- if condition is true then sttement is true otherwise false.
+- syntax :- 
+if (condition){
+    statement
+} else {
+    statement
+}
+
+3. if-elif condition or switch condition
+
+- its check the comdition multiple times, where the condition is true, it returns the statement.
+- if else if case. else condition if the default condition so if the condition doesn't satisfy any of the case then he print the default condition.
+
+- syntax :- 
+if (condition){
+    statement
+} else if(condition){
+    statement
+} else if(condition){
+    statement
+} else {
+    statement
+}
+
+practice task :- 
+
+write a code which can give grades to student according to their score. 
+1. 80-100 (A)
+2. 70-79 (B)
+3. 60-69 (C)
+4. 50-59 (D)
+5. 0-49 (fail - go for aganwadi)
+
+
+
 
 
 
---
+++ basic-js/condition.js
@@ -0,0 +1,71 @@
+// if condition 
+
+// let age = 21;
+// if(age >= 18){
+//     console.log("you can give the vote")
+// }
+
+// if-else condition 
+
+// let age = 10;
+
+// if(age >= 18){
+//     console.log("you can give the vote") 
+// } else {
+//     console.log("you can't give the vote")
+// }
+
+// if-elif statement
+
+// let mode = "green"
+// let color;
+
+// if(mode === "dark"){
+//     color = "black"
+// } else if(mode === "blue"){
+//     color = "blue"
+// }  else if(mode === "pink"){
+//     color = "pink"
+// } else {
+//     color = "can't define"
+// }
+
+// console.log(color)
+
+
+// practice task-answer
+
+// let score = 67;
+// let grade;
+
+// if (score >= 90 && score <= 100){
+//     grade = "A"
+// } else if (score >= 70 && score <= 79){
+//     grade = "B"
+// } else if (score >= 60 && score <= 69){
+//     grade = "C"
+// } else if (score >= 50 && score <= 59){
+//     grade = "D"
+// } else if (score >= 0 && score <= 49){
+//     grade = "F"
+// }
+
+// console.log("according to your score the grade is ", grade)
+
+// switch case 
+
+var fruits = "grapes";
+switch(fruits){
+    case "banana":
+        console.log("banana is good")
+        break;
+    case "orange":
+        console.log("orange is good")
+        break;
+    case "apple":
+        console.log("apple is good")
+        break;
+    default:
+        console.log("all are bad")
+}
+
